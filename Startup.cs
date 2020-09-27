8350f986f54a8f9f440286082d9e0d0afdc0aaf7
Marco Antonio Manrique Acha <manriqueacham@gmail.com>
2020-09-26 23:23:33 -0500

    5 Entidades

---
+++ Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EasyStory.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                // options.UseInMemoryDatabase("supermarket-api-in-memory");
+                options.UseMySQL(Configuration.GetConnectionString("MySQLConnection"));
+            });
+
+            // Repositories
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductTagRepository, ProductTagRepository>();
+            services.AddScoped<ITagRepository, TagRepository>();
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            // Unit Of Work
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            // Services
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductTagService, ProductTagService>();
+            services.AddScoped<ITagService, TagService>();
+
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddCustomSwagger();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseCustomSwagger();
+        }
+    }
+}
