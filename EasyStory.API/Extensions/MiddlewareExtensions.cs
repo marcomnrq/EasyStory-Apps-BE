788a1e1b62f3f60b9a88d0a6814e60ffbaf0db0e
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2020-10-23 21:19:31 -0500

    Fixed project structure

---
+++ EasyStory.API/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace EasyStory.API.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "EasyStory API",
+                    Version = "v1",
+                    Description = "EasyStory RESTful API"
+                });
+                c.EnableAnnotations();
+            });
+            return services;
+        }
+
+        public static IApplicationBuilder UseCustomSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "api-docs/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/api-docs/v1/swagger.json", "Supermarket API V1");
+                c.RoutePrefix = "api-docs/v1";
+            });
+            return app;
+        }
+    }
+}
+
